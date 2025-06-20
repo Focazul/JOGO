e1b208bd1c9f428c34b5b1bffe942b47f761f18b
Focazul <marcelo_juninho.891012@live.com>
2025-06-20 19:59:21 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,102 @@
+const questions = [
+    {
+        question: "Quantas voltas vai na casquinha?",
+        options: [
+            "3,5",   // 0 (1ª opção)
+            "2,5",   // 1 (2ª opção)
+            "4,5",   // 2 (3ª opção)
+            "5,5"    // 3 (4ª opção)
+        ],
+        correctIndex: 0
+    },
+    {
+        question: "Quantos picles vai no BigMac?",
+        options: [
+            "1", // 0 (1ª opção)
+            "2", // 1 (2ª opção)
+            "3", // 2 (3ª opção)
+            "4"  // 3 (4ª opção)
+        ],
+        correctIndex: 1
+    },
+    {
+        question: "Qual a temperatura do reservatorio de mix?",
+        options: [
+            "3° a 6°",   // 0 (1ª opção)
+            "2° a 5°",   // 1 (2ª opção)
+            "1° a 4°",   // 2 (3ª opção)
+            "-18° a -23°"// 3 (4ª opção)
+        ],
+        correctIndex: 2
+    },
+    {
+        question: "Quantidade de alface em cada lado do BigMac?",
+        options: [
+            "12g", // 0 (1ª opção)
+            "14g", // 1 (2ª opção)
+            "20g", // 2 (3ª opção)
+            "24g"  // 3 (4ª opção)
+        ],
+        correctIndex: 0
+    },
+    {
+        question: "Quantas pedras de gelo vai no refrigerante pequena?",
+        options: [
+            "3",  // 0 (1ª opção)
+            "15", // 1 (2ª opção)
+            "10", // 2 (3ª opção)
+            "5"   // 3 (4ª opção)
+        ],
+        correctIndex: 3 // 4ª opção (corrigido de 4 para 3)
+    },
+    {
+        question: "Quantas pedras de gelo vai no refrigerante média?",
+        options: [
+            "3",  // 0 (1ª opção)
+            "15", // 1 (2ª opção)
+            "10", // 2 (3ª opção)
+            "5"   // 3 (4ª opção)
+        ],
+        correctIndex: 2
+    },
+    {
+        question: "Quantas pedras de gelo vai no refrigerante grande?",
+        options: [
+            "3",  // 0 (1ª opção)
+            "15", // 1 (2ª opção)
+            "10", // 2 (3ª opção)
+            "5"   // 3 (4ª opção)
+        ],
+        correctIndex: 1
+    },
+    {
+        question: "Qual tempo de vida da batata na estufa em minutos?",
+        options: [
+            "7'", // 0 (1ª opção)
+            "8'", // 1 (2ª opção)
+            "9'", // 2 (3ª opção)
+            "6'"  // 3 (4ª opção)
+        ],
+        correctIndex: 0
+    },
+    {
+        question: "Quantas tiros de cobertura vai no top sunday?",
+        options: [
+            "4", // 0 (1ª opção)
+            "3", // 1 (2ª opção)
+            "1", // 2 (3ª opção)
+            "2"  // 3 (4ª opção)
+        ],
+        correctIndex: 3
+    },
+    {
+        question: "Quantas Oz de molho vai no Cheddar?",
+        options: [
+            "2 Oz",   // 0 (1ª opção)
+            "1 Oz",   // 1 (2ª opção)
+            "2/3 Oz", // 2 (3ª opção)
+            "1/3 Oz"  // 3 (4ª opção)
+        ],
+        correctIndex: 1 
+    }
+];
