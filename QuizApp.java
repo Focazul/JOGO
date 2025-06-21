9ca3e0f13cbef6c31e4253100ef4bd9973f5bc08
Focazul <marcelo_juninho.891012@live.com>
2025-06-21 17:51:56 -0300

    Update and rename jogo.html to QuizApp.java

---
+++ QuizApp.java
@@ -0,0 +1,18 @@
+// ---------------------------------------------
+// IMPORTS E DEFINIÇÃO DA CLASSE PRINCIPAL
+// ---------------------------------------------
+package quiz.main;
+
+import quiz.ui.QuizUI;
+
+// ---------------------------------------------
+// CLASSE DE ENTRADA DO PROGRAMA
+// ---------------------------------------------
+public class QuizApp {
+    // -------------------
+    // MÉTODO MAIN: Inicia a interface do quiz
+    // -------------------
+    public static void main(String[] args) {
+        QuizUI.main(args);
+    }
+}
